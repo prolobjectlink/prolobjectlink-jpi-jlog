0df419f716293f9cef9f60f58f2d019c6cfdee13
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-02-03 16:49:05 -0400

    Query builder OK

--- src/main/java/org/prolobjectlink/prolog/jlog/JLogQuery.java
+++ src/main/java/org/prolobjectlink/prolog/jlog/JLogQuery.java
@@ -280,6 +280,14 @@ public final class JLogQuery extends AbstractQuery implements PrologQuery {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((solution == null) ? 0 : solution.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -294,22 +302,7 @@ public final class JLogQuery extends AbstractQuery implements PrologQuery {
 		} else if (!solution.equals(other.solution)) {
 			return false;
 		}
-		if (vector == null) {
-			if (other.vector != null)
-				return false;
-		} else if (!vector.equals(other.vector)) {
-			return false;
-		}
 		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((solution == null) ? 0 : solution.hashCode());
-		result = prime * result + ((vector == null) ? 0 : vector.hashCode());
-		return result;
 	}
 
 	public void dispose() {
